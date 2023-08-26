19d3945b44169435bd34b3c3db689621d8debc98
Anirban <anirbanchondrobanai@gmail.com>
2023-08-27 00:33:48 +0600

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
+"use client"
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import "../components/i18n"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
